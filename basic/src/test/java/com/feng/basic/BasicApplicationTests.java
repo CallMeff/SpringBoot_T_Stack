cfa132e24f8655c632312f5edcd3dea65c444a9a
tianfengluyao <386774782@qq.com>
2018-11-14 10:18:31 +0800

    basic:单元测试

--- basic/src/test/java/com/feng/basic/BasicApplicationTests.java
+++ basic/src/test/java/com/feng/basic/BasicApplicationTests.java
@@ -1,16 +1,24 @@
 package com.feng.basic;
 
+import com.feng.basic.controller.HelloController;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BasicApplicationTests {
 
+    @Resource
+    private HelloController helloController;
+
     @Test
     public void contextLoads() {
+        Assert.assertEquals(helloController.sayHello(),"hello,springboot");
     }
 
 }
